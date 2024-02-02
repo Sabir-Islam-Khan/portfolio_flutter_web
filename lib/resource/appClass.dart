f91dca1133306ea6741a512fe24d24808d075c74
Sabir-Islam-Khan <sabir.islam14@gmail.com>
2024-02-02 17:31:19 +0600

    work section

--- lib/resource/appClass.dart
+++ lib/resource/appClass.dart
@@ -4,7 +4,6 @@ import 'package:portfolio/model/models.dart';
 import 'package:portfolio/resource/styles.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 enum ScreenType { mobile, tab, web }
 
 class AppClass {
@@ -28,44 +27,51 @@ class AppClass {
 
   List<WorkModel> projectList = [
     WorkModel(
-        projectTitle: "SafeC19",
+        projectTitle: "TakeCare",
         projectContent:
-            "A fully automatic temperature and face mask detection solution for a completely contactless hand sanitizer. A solution so unique that it has integrated door access control if monitoring parameters are normal. An image is still captured and uploaded in cloud server on its own, as reference for auditing.",
-        tech1: "Android",
-        tech2: "Google-ML",
-        tech3: "Java"),
-    WorkModel(
-        projectTitle: "Hermarts",
-        projectContent:
-            "Hermarts is an Ecommerce website/mobile application specially designed to encourage and takeout the hidden talent of women candidates, who never gets opportunities to explore their talents. It is developed with user-centric features to help the users with integrated InApp payment mode with high level security.",
-        tech1: "Android",
-        tech2: "RazorPay"),
-    WorkModel(
-        projectTitle: "WT-IoT",
-        projectContent:
-            '''It’s a water treatment process controller which can control all the sensors, calibration process, and staff management.''',
-        tech1: "Android",
-        tech2: "TCP",
-        tech3: "API"),
-    WorkModel(
-        projectTitle: "AutoStabilizer",
-        projectContent:
-            '''It is used to connect to a stabilizer and read the inlet and outlet voltage and several other parameters of the Stabilizer. This application will also set up the stabilizer hardware to connect the stabilizer to the Wi-Fi network. And with some features like PIR Sensor, Control Stabilizer with schedule, Standby Mode, Control Stabilizer with MobileData via AWS, and With WiFi via TCP Connection.''',
-        tech1: "Android",
-        tech2: "MQTT"),
-    WorkModel(
-        projectTitle: "PAT",
-        projectContent:
-            '''This application is to store and record the data of tested production product information for future reference. This application can reduce the time and ensure the performance and quality of the QA/QC people. This application let the super admin to track the workers and their performance. Through this application, we can calculate the time spent on every test that was taken to complete by the worker.''',
+            "Leading teli-medicine service provider of Bangladesh. Worked as a senior developer to optimize and scale their already made platform. It was indeed a great experience. TakeCare is now growing rapidly and hopefully soon will be a big name in our country.",
         tech1: "Flutter",
-        tech2: "NodeJs",
-        tech3: "Flutter-Windows"),
+        tech2: "Firebase",
+        tech3: "GeoCoding"),
     WorkModel(
-        projectTitle: "AVM",
+      projectTitle: "MatchMap",
+      projectContent:
+          "MatchMap is a sports event planning and hosting platform. You can host events, find sponsors and also buy tickets of your desired events. Its a german based product and I worked as a freelance developer there.",
+      tech1: "Flutter",
+      tech2: "Stripe",
+      tech3: "GraphQL",
+    ),
+    WorkModel(
+        projectTitle: "Zyco",
         projectContent:
-            '''AVM - Automatic Vending Machine is one of Pradeep's stainless steel product. The project is aimed at automating the beverage vending process. It can be done through hardware that controls the main engines and a mobile app (which acts like a PCB display for the unit) that is used to personalize and make beverage. It can also perform several process like Brewing, Boiling,cleansing drum etc . The Engines (hardware) and Mobiel Application communicate using Bluetooth.''',
-        tech1: "Android",
-        tech2: "Tablet"),
+            '''Zyco is business to business Taxi service provider based on Netherlands. I worked as a Flutter Developer to create their app. Communicating with backend developers and UI/UX designers were by go to task. I mainly worked with front end and API integration.''',
+        tech1: "REST",
+        tech2: "Microservices",
+        tech3: "Flutter"),
+    WorkModel(
+      projectTitle: "Bus Location Tracker",
+      projectContent:
+          '''One of my hobby project. I created this with one of my friend. We coded a MQTT signalling device that sends location data to our real time server. We made a flutter app too where users can search vehicle and see its real time location in a snap.''',
+      tech1: "Android",
+      tech2: "MQTT",
+      tech3: "RealTime DB",
+    ),
+    WorkModel(
+      projectTitle: "Duah",
+      projectContent:
+          '''This native android app using built using Java for a client. This is a digital Quran. You can search any verse easily, see translation in any language and also use its TTS functionality to listen to any verse. Its available on playstore only.''',
+      tech1: "Java",
+      tech2: "Firebase",
+      tech3: "Text to Speech",
+    ),
+    WorkModel(
+      projectTitle: "Friend In Need",
+      projectContent:
+          '''Another native android app made with Java. I made this for national science congress 2018 and became champion. This app can save your emergency contact and when in danger, by pressing dedicated buttons, it will send users realtime location without any internet to the emergency contacts and also to the nearest Police Station.''',
+      tech1: "Java",
+      tech2: "Android",
+      tech3: "Geo-Location",
+    ),
   ];
 
   factory AppClass() {
@@ -117,11 +123,9 @@ class AppClass {
 
   Future<bool> sendEmail(name, contact, msg) async {
     var url = Uri.https('hbk-portfolio-mailer.web.app', '/sendMail');
-    var response = await post(url, body: {
-      "name": name,
-      "contactInfo": contact,
-      "message": msg
-    }).timeout(Duration(seconds: 10));
+    var response = await post(url,
+            body: {"name": name, "contactInfo": contact, "message": msg})
+        .timeout(Duration(seconds: 10));
     print(response.body);
     return response.statusCode == 200;
   }
--- lib/view/work/workLarge.dart
+++ lib/view/work/workLarge.dart
@@ -41,7 +41,7 @@ class _WorkWebState extends ConsumerState<WorkWeb> {
         Padding(
           padding: const EdgeInsets.only(top: 8.0),
           child: Text(
-            'view the archives',
+            'Here are few of my projects tha Im proud of',
             style: TextStyle(
                 color: AppColors().neonColor,
                 fontSize: 15,
@@ -134,7 +134,9 @@ class _WorkWebState extends ConsumerState<WorkWeb> {
         return Container(
           margin: EdgeInsets.all(isHovered ? 8.0 : 0.0),
           child: Tooltip(
-            message: AppClass().projectList[index].projectTitle.toString() + "\n\n" + AppClass().projectList[index].projectContent.toString(),
+            message: AppClass().projectList[index].projectTitle.toString() +
+                "\n\n" +
+                AppClass().projectList[index].projectContent.toString(),
             padding: EdgeInsets.all(20),
             margin: EdgeInsets.all(AppClass().getMqWidth(context) * 0.1),
             waitDuration: Duration(seconds: 3),
@@ -166,7 +168,8 @@ class _WorkWebState extends ConsumerState<WorkWeb> {
                           'assets/svg/externalLink.svg',
                           width: 22,
                           height: 22,
-                          color: isHovered ? AppColors().neonColor : Colors.white,
+                          color:
+                              isHovered ? AppColors().neonColor : Colors.white,
                         ),
                       ],
                     ),
@@ -175,7 +178,10 @@ class _WorkWebState extends ConsumerState<WorkWeb> {
                       child: Row(
                         children: [
                           Text(
-                            AppClass().projectList[index].projectTitle.toString(),
+                            AppClass()
+                                .projectList[index]
+                                .projectTitle
+                                .toString(),
                             textAlign: TextAlign.left,
                             style: GoogleFonts.robotoSlab(
                                 color: isHovered
@@ -192,7 +198,10 @@ class _WorkWebState extends ConsumerState<WorkWeb> {
                       child: Padding(
                         padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
                         child: Text(
-                          AppClass().projectList[index].projectContent.toString(),
+                          AppClass()
+                              .projectList[index]
+                              .projectContent
+                              .toString(),
                           style: GoogleFonts.roboto(
                             color: AppColors().textLight,
                             letterSpacing: 1,
