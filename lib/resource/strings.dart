7c6b6fc4223892217bc528ea47e2e8cbd66eed45
Sabir-Islam-Khan <sabir.islam14@gmail.com>
2024-02-02 16:35:21 +0600

    about complete

--- lib/resource/strings.dart
+++ lib/resource/strings.dart
@@ -5,23 +5,23 @@ class Strings {
   static final whatIdo =
       '''If i can think it, I can code it.'''; // add . at the end
   static final introAbout =
-      '''I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products as the CEO and founder of ''';
+      '''I’m a software developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products as the CEO and founder of ''';
   static final currentOrgName = '''AsteriskLab'''; // add . at the end
 
   /* About screen contents */
   static final aboutPara1 =
-      '''Hello! Myself Jeeva and I enjoy creating things that live on the internet. My interest in mobile/web development started back in 2019 when I decided to clone some applications — turns my passion into profession.''';
+      '''Hello! I am Sabir and I enjoy creating things that live on the internet. I started coding when I was 14 and felt immense pleasure in it. I created many passion project in my school days. Later, I decided to turn my passion into my profession.''';
   static final aboutPara2 =
-      '''Fast-forward to today, and I’ve had the privilege of working for a good organization, a product-based, a huge corporation, and a student-led working atmosphere. My main focus these days is building accessible, inclusive products and digital experiences at Netaccess for a variety of clients.''';
+      '''Fast-forward to today, and I’ve had the privilege of working with many good organizations around the world. Starting from remotely working in a company from Singapore, I hopped around and gained experience by working with companies from Germany, Netherlands, England, USA and Canada. Now, my main focus these days is building accessible, inclusive products and digital experiences at AsteriskLab for a variety of clients.''';
   static final aboutPara3 =
       '''I also freelance for various clients across the world. If you've any ideas about creating a project or something, feel free to contact me!''';
 
   static final techIntro =
       '''Here are a few technologies I’ve been working with recently:''';
   static final tech1 = ''' Flutter'''; // space for manual padding
-  static final tech2 = ''' NodeJs'''; // space for manual padding
+  static final tech2 = ''' Java'''; // space for manual padding
   static final tech3 = ''' GraphQL'''; // space for manual padding
-  static final tech4 = ''' Swift'''; // space for manual padding
+  static final tech4 = ''' PostgreSQL'''; // space for manual padding
 
   /* Exp screen contents */
   static final expCompName1 = '''Techno Kryon''';
